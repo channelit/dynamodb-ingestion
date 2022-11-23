2d94b6dd594c458c2c0099f42cbde39374671658
Hardik Patel <hpatel@channelit.biz>
2022-11-22 20:55:47 -0500

    error handling

--- ingest_using_config/main.py
+++ ingest_using_config/main.py
@@ -1,8 +1,11 @@
-import boto3
+import os
+
+from boto3 import client
+import botocore
 import yaml
 
-dynamodb = boto3.client('dynamodb')
-config_file = 'config.yml'
+dynamodb = client('dynamodb')
+config_file = '/Users/hardikpatel/workbench/projects/ingestion/config.yml'
 csv_file = 'user.csv'
 tableName = "table"
 
@@ -14,42 +17,55 @@ def get_config(filename):
 
 
 def main():
+    print(config_file)
     docs = get_config(config_file)
     for doc in docs:
-        import_to_dynamo(doc)
+        response = import_to_dynamo(doc)
+        print(response)
+        if 'ImportTableDescription' in response:
+            print('Arn: {}'.format(response['ImportTableDescription']['ImportArn']))
+        else:
+            print('Error: {}'.format(response['Message']))
     return print("Done")
 
 
 def import_to_dynamo(doc):
-    response = dynamodb.import_table(
-        S3BucketSource={
-            'S3Bucket': doc['s3Bucket'],
-            'S3KeyPrefix': doc['s3KeyPrefix']
-        },
-        InputFormat='CSV',
-        InputFormatOptions={
-            'Csv': {
-                'Delimiter': ',',
+    response = ""
+    try:
+        response = dynamodb.import_table(
+            S3BucketSource={
+                'S3Bucket': doc['s3Bucket'],
+                'S3KeyPrefix': doc['s3KeyPrefix']
+            },
+            InputFormat='CSV',
+            InputFormatOptions={
+                'Csv': {
+                    'Delimiter': ',',
+                }
+            },
+            InputCompressionType='NONE',
+            TableCreationParameters={
+                'TableName': doc["tableName"],
+                'AttributeDefinitions': [
+                    {
+                        'AttributeName': doc['keyField'],
+                        'AttributeType': 'S'
+                    },
+                ],
+                'KeySchema': [
+                    {
+                        'AttributeName': doc['keyField'],
+                        'KeyType': 'HASH'
+                    },
+                ],
+                'BillingMode': 'PAY_PER_REQUEST'
             }
-        },
-        InputCompressionType='NONE',
-        TableCreationParameters={
-            'TableName': doc["tableName"],
-            'AttributeDefinitions': [
-                {
-                    'AttributeName': doc['keyField'],
-                    'AttributeType': 'S'
-                },
-            ],
-            'KeySchema': [
-                {
-                    'AttributeName': doc['keyField'],
-                    'KeyType': 'HASH'
-                },
-            ],
-            'BillingMode': 'PAY_PER_REQUEST'
-        }
-    )
+        )
+    except botocore.exceptions.ClientError as error:
+        response = error.response['Error']
+    except botocore.exceptions.ParamValidationError as error:
+        raise ValueError('The parameters you provided are incorrect: {}'.format(error))
+    return response
 
 
 if __name__ == '__main__':
