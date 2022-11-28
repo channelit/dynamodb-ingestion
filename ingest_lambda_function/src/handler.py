a203c0920118b6c9082afc48b2bf5247ad870303
Hardik Patel <hpatel@channelit.biz>
2022-11-28 10:43:58 -0500

    table overwrite

--- ingest_lambda_function/src/handler.py
+++ ingest_lambda_function/src/handler.py
@@ -7,8 +7,7 @@ def handler(event, context):
     s3_key_prefix: str = event.get("s3KeyPrefix") or "none"
     table_name: str = event.get("tableName") or "none"
     key_field: str = event.get("keyField") or "none"
-    delete_if_exists: bool
-    event.get("deleteIfExists") or False
+    delete_if_exists: bool = event.get("deleteIfExists") or False
     response = IngesterService(table_name=table_name).import_to_dynamo(s3_bucket=s3_bucket, s3_key_prefix=s3_key_prefix,
                                                                        key_field=key_field,
                                                                        delete_if_exists=delete_if_exists)
